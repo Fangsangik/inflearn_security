2e401992a35475ada752a09b104b85022f48acf4
Fangsangik <hwangsangik@gmail.com>
2024-09-09 19:01:14 +0900

    feat = "anonymous"

--- src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -47,38 +47,13 @@ public class SecurityConfig {
                 //SecurityFilterChainConfiguration -> filter 조건 성립 안됨
                 //SecurityFilterChain -> Bean을 생성했기 때문 -> Bean이 없을 경우에만 성립된다. -> 사용자가 설정한 쪽으로 오게 된다.
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/logoutSuccess").permitAll()
+                        .requestMatchers("/anonymous").hasRole("GUEST")
+                        .requestMatchers("/anonymousContext", "/authentication").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
-                //.csrf(csrf -> csrf.disable()) //csrf 방식이 disable -> post 방식 뿐만 아니라 get 방식도 적용이 가능
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "POST")) //get 방식도 가능
-                        .logoutSuccessUrl("/logoutSuccess") // 해당하는 Mapping 생성 logoutRequestMatcher 처리 안하면 post 방식만 가능
-                        .logoutSuccessHandler(new LogoutSuccessHandler() {
-                            @Override
-                            public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                                response.sendRedirect("/logoutSuccess");
-                                //복잡한 경로 -> handler 사용
-                            }
-                        })
-                        .deleteCookies("JSESSIONID", "remember-me") //보통적으로 JSESSIONID는 삭제
-                        .invalidateHttpSession(true) // logout시 session 무효화
-                        .clearAuthentication(true) //securityContext 안에 있는 authentication 객체를 제거
-                        .addLogoutHandler(new LogoutHandler() {
-                            @Override
-                            public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-                                //세션 무효화
-                                HttpSession session = request.getSession();
-                                session.invalidate();
-                                //시큐리티 컨택스트 안에있는 authentication을 제거 가능 / 기존 authenticaiton 객체가 제거
-                                SecurityContextHolder.getContextHolderStrategy().getContext().setAuthentication(null);
-                                SecurityContextHolder.getContextHolderStrategy().clearContext(); //SecurityContextHolder 또한 clear
-                            }
-                        })
-
-                        .permitAll() //logoutProc
-                );
+                .anonymous(anonymous -> anonymous
+                        .principal("guest")
+                        .authorities("ROLE_GUEST"));
 
 
         return http.build(); // securityFilterChain 생성된다.
